884a50414fcc539a614964c4dfc786f08ed00285
Aleksei Vergasov <ASVergasov@gmail.com>
2022-12-31 14:04:53 +0600

    Change languages

--- conftest.py
+++ conftest.py
@@ -2,26 +2,26 @@ import time
 import pytest
 from selenium import webdriver
 
-url_eng = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/"
-url_rus = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
+url_es = "http://selenium1py.pythonanywhere.com/es/catalogue/coders-at-work_207/"
+url_fr = "http://selenium1py.pythonanywhere.com/fr/catalogue/coders-at-work_207/"
 
 
 def pytest_addoption(parser):
-    parser.addoption('--lang_type', action='store', default='None', help="Choose language: english or russian")
+    parser.addoption('--language', action='store', default='None', help="Choose language: english or russian")
 
 
 @pytest.fixture(scope="function")
 def driver(request):
-    lang_type = request.config.getoption('--lang_type')
+    language = request.config.getoption('--language')
     print("\nStart browser test..")
     driver = webdriver.Chrome()
-    if lang_type == "english":
-        driver.get(url_eng)
-    elif lang_type == "russian":
-        driver.get(url_rus)
+    if language == "es":
+        driver.get(url_es)
+    elif language == "fr":
+        driver.get(url_fr)
     else:
-        raise pytest.UsageError("-- lang_type should be english or russian")
+        raise pytest.UsageError("-- language should be english or russian")
     yield driver
     print("\nQuit browser..")
-    time.sleep(25)
+    time.sleep(30)
     driver.quit()
--- test_items.py
+++ test_items.py
@@ -7,8 +7,12 @@ from selenium.webdriver.support.wait import WebDriverWait
 @pytest.mark.lang
 def test_lang(driver):
     #поиск элемента на странице
-    WebDriverWait(driver,5).until(EC.presence_of_element_located((By.XPATH, "//button[@class ='btn btn-lg btn-primary btn-add-to-basket']")))
+    button = WebDriverWait(driver,5).until(EC.presence_of_element_located((By.XPATH, "//button[@class ='btn btn-lg btn-primary btn-add-to-basket']")))
 
+    if :
+        assert button == "Añadir al carrito", "Should be Añadir al carrito"
+    else:
+        assert button == "Ajouter au panier", "Should be Ajouter au panier"
 
 
 
